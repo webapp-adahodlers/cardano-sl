92751f9a45bf5a70e1afbdc01db2ecc4189da7b7
Samuel Leathers <samuel.leathers@iohk.io>
2018-05-18 05:20:45 -0400

    [CDEC-271] demo-cluster: add relay node and use normal wallet

--- scripts/launch/connect-to-cluster/default.nix
+++ scripts/launch/connect-to-cluster/default.nix
@@ -14,6 +14,7 @@
 , additionalNodeArgs ? ""
 , confKey ? null
 , relays ? null
+, debug ? false
 , extraParams ? ""
 }:
 
@@ -24,6 +25,7 @@ with localLib;
 # TODO: DEVOPS-462: exchanges should use a different topology
 
 let
+  ifDebug = localLib.optionalString (debug);
   environments = {
     mainnet = {
       relays = "relays.cardano-mainnet.iohk.io";
@@ -35,6 +37,7 @@ let
     };
     demo = {
       confKey = "dev";
+      relays = "127.0.0.1";
     };
     override = {
       inherit relays confKey;
@@ -68,9 +71,11 @@ in pkgs.writeScript "${executable}-connect-to-${environment}" ''
   if [[ "$1" == "--delete-state" ]]; then
     echo "Deleting ${stateDir} ... "
     rm -Rf ${stateDir}
+    shift
   fi
-  if [[ "$2" == "--runtime-args" ]]; then
-    RUNTIME_ARGS=$3
+  if [[ "$1" == "--runtime-args" ]]; then
+    RUNTIME_ARGS=$2
+    shift 2
   else
     RUNTIME_ARGS=""
   fi
@@ -103,6 +108,7 @@ in pkgs.writeScript "${executable}-connect-to-${environment}" ''
     --logs-prefix "${stateDir}/logs"                               \
     --db-path "${stateDir}/db"   ${extraParams}                    \
     ${ ifWallet "--wallet-db-path '${stateDir}/wallet-db'"}        \
+    ${ ifDebug "--wallet-debug"}                                   \
     --keyfile ${stateDir}/secret.key                               \
     ${ ifWallet "--wallet-address ${walletListen}" }               \
     ${ ifWallet "--wallet-doc-address ${walletDocListen}" }        \

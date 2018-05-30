02387795a79295e5325a7580f267b8efd038da74
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-05-30 15:42:13 +0100

    [DEVOPS-398] Add genesis-hash to cardano-sl-tools

---
+++ tools/src/genesis-hash/Main.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import System.Exit (die)
+import System.Environment (getArgs)
+import System.IO (stdin)
+import Data.ByteString.Lazy.Char8 as L8
+import Text.JSON.Canonical (JSValue, parseCanonicalJSON, renderCanonicalJSON)
+import Crypto.Hash (Digest, hashlazy)
+import Crypto.Hash.Algorithms (Blake2b_256)
+
+main :: IO ()
+main = do
+  f <- parseArgs
+  val <- readJSON f
+  print (blakeHash val)
+
+parseArgs :: IO (Maybe FilePath)
+parseArgs = getArgs >>= \case
+  ["-"] -> pure Nothing
+  [f] -> pure (Just f)
+  _ -> die "usage: genesis-hash INFILE.json"
+
+readJSON :: Maybe FilePath -> IO JSValue
+readJSON f = do
+  bs <- case f of
+    Just f -> L8.readFile f
+    Nothing -> L8.hGetContents stdin
+  case parseCanonicalJSON bs of
+    Right v -> pure v
+    Left e -> die e
+
+blakeHash :: JSValue -> Digest Blake2b_256
+blakeHash = hashlazy . renderCanonicalJSON
